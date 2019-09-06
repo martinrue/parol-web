98305a3d1f385437129cf727b9156c855a34a6c4
Martin Rue <hello@martinrue.com>
2019-09-06 23:42:34 +0100

    Bump to v0.0.3

--- js/index.js
+++ js/index.js
@@ -348,7 +348,7 @@ window.parol = (() => {
   };
 
   const displayVersion = () => {
-    console.log("Parol version: v0.0.2");
+    console.log("Parol version: v0.0.3");
   };
 
   const init = () => {
